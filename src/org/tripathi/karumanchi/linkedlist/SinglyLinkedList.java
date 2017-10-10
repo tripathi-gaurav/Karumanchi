59f931a476ec15ae437d7d45e42aea74e6a08be3
Gaurav Tripathi <gaurav@tripathi.org>
2017-10-10 13:26:39 -0400

    Added Insert in between Linked List. (Chapter 2, page 43, Singly Linked Lists)

--- src/org/tripathi/karumanchi/linkedlist/SinglyLinkedList.java
+++ src/org/tripathi/karumanchi/linkedlist/SinglyLinkedList.java
@@ -33,5 +33,68 @@ public class SinglyLinkedList {
 		return newHeadNode;
 	}
 	
+	ListNode InsertNodeAtEndOfLinkedList(ListNode headNode, int data) {
+		ListNode newNode = new ListNode(data);
+		newNode.setNext(null);
+		ListNode currentNode = headNode;
+		/*traverse the list till you reach the last element
+		 * the first implementation that i came up with. 
+		 * one should delete the dead code below.
+		 * while(true) {
+			if(currentNode.getNext() != null) {
+				currentNode = currentNode.getNext();
+			}else {
+				break;
+			}
+		}*/
+		// i feel the below implementation is more readable
+		while( currentNode.getNext() != null ) {
+			currentNode = currentNode.getNext();
+		}
+		//now that currentNode is the last node, set its next pointer to the new node
+		currentNode.setNext(newNode);
+		return headNode;
+		//Time Complexity: O(n)
+		
+	}
+	
+	
+	//Now we insert a node in between the linked list at a specified position
+	//the node to be inserted is provided by user now
+	ListNode InsertInLinkedList(ListNode headNode, ListNode nodeToInsert, int position) {
+		if( headNode==null ) {
+			//if the headNode itself is null, let us set the newNode as the headNode and be sorted
+			return nodeToInsert;
+		}
+		Integer size = ListLength(headNode);
+		if( position > size+1 || position < 1 ) {
+			System.out.println("Invalid position specified. Valid positions are between 1 and " + size+1);
+			return headNode;
+		}
+		// if position is 1, that means insertion is happening before the headnode
+		// (I) to insert before the headnode, we will have to use set the nodeToInsert's next pointer 
+		// (II) also we return the nodeToInsert in order to change the headNode to point to the new one
+		// for everything else, we can traverse from the headNode to the desired position.
+		if( position == 1 ) {
+			nodeToInsert.setNext(headNode); //(I)
+			return nodeToInsert; //(II)
+		}else {
+			//all other cases, traverse
+			ListNode previousNode = headNode;
+			// we start with 1 as in head position, go up to position - 1 
+			// pos - 1 will be the node AFTER which insertion is done. 
+			// if we go up to position, then we can't do anything with that node as that node should actually 
+			//						...be the next node after the nodeToInsert
+			for(int i=1;i<position-1;i++) {
+				previousNode = headNode.getNext(); 
+			}
+			ListNode nextNode = previousNode.getNext(); 
+			previousNode.setNext(nodeToInsert);
+			nodeToInsert.setNext(nextNode);
+		}
+		return headNode;
+		//Time Complexity: O(n) for insertion at worst
+		//Space Complexity: O(1) for the temp variable
+	}
 
 }
