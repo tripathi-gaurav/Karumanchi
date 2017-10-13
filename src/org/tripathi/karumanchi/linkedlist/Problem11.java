20cc650660d5a2e060767c10199be6b8a3a1368c
Gaurav Tripathi <gaurav@tripathi.org>
2017-10-13 11:07:36 -0400

    Two more solutions using Floyd's algorithm for find loops

---
+++ src/org/tripathi/karumanchi/linkedlist/Problem11.java
@@ -0,0 +1,45 @@
+package org.tripathi.karumanchi.linkedlist;
+
+//find if loop exists by Floyd's algorithm and then find the node at which loop exists
+//we use the same fastPtr and slowPtr logic as in Problem9
+//once loop is found then we re-initialize the slowPtr to head
+//and from there on, increment both fastPtr and slowPtr by one till they are equal
+public class Problem11 {
+
+	public static void main(String[] args) {
+		SinglyLinkedList sll = new SinglyLinkedList();
+		ListNode headNode = Problem6.getLinkedListWithLoop(sll);
+		Problem11 solution = new Problem11();
+		ListNode nodeAtWhichLoopExists =  solution.findNodeAtWhichLoopExists(headNode);
+		if(nodeAtWhichLoopExists == headNode) {
+			System.out.println("No loop");
+		}else {
+			System.out.println(nodeAtWhichLoopExists.getData());
+		}
+	}
+
+	ListNode findNodeAtWhichLoopExists(ListNode headNode) {
+
+		ListNode fastPtr = headNode, slowPtr = headNode;
+		Boolean loopExists = false;
+		while(fastPtr.getNext() != null) {
+			fastPtr = fastPtr.getNext().getNext();
+			slowPtr = slowPtr.getNext();
+			if(fastPtr==slowPtr) {
+				loopExists = true;
+				break;
+			}
+		}
+		
+		if(loopExists) {
+			slowPtr = headNode;
+			while(fastPtr != slowPtr) {
+				fastPtr = fastPtr.getNext();
+				slowPtr = slowPtr.getNext();
+			}
+			return fastPtr;
+		}
+		
+		return headNode;
+	}
+}
---
+++ src/org/tripathi/karumanchi/linkedlist/Problem14.java
@@ -0,0 +1,58 @@
+package org.tripathi.karumanchi.linkedlist;
+
+
+//find if there is cycle(loop) in the linked list
+//if there is a cycle, find the length of the cycle
+
+public class Problem14 {
+	public static void main(String[] args) {
+		SinglyLinkedList sll = new SinglyLinkedList();
+		ListNode headNode = Problem6.getLinkedListWithLoop(sll);
+		Problem14 solution = new Problem14();
+		ListNode fastPtr = headNode, slowPtr = headNode;
+		Integer count = 1;
+		
+		Boolean loopExists = false;
+		while(fastPtr.getNext() != null) {
+			fastPtr = fastPtr.getNext().getNext();
+			slowPtr = slowPtr.getNext();
+			if(slowPtr == fastPtr) {
+				loopExists = true;
+				//break;
+			}
+			
+			if(loopExists) {
+				fastPtr = fastPtr.getNext(); //since count is already initialized to 0, so no problem.
+				while(fastPtr != slowPtr) {
+					count++;
+					fastPtr = fastPtr.getNext();
+				}
+				break;
+				/* 
+				 //re-initialized slowPtr to headNode and then found the start of the cycle/loop
+				 //once at start, then ran a loop while incrementing a counter till the slowPtr reach back
+				 //to the start of the loop
+				 //better method was in the book. instead of finding the start ofthe loop
+				 //leave the slowPtr where it is, increment the fastPtr till it equals the slowPtr 
+				slowPtr = headNode;
+				while(slowPtr != fastPtr) {
+					slowPtr = slowPtr.getNext();
+					fastPtr = fastPtr.getNext();
+				}
+				
+				ListNode nodeAtStartOfCycle = fastPtr;
+				slowPtr = slowPtr.getNext();
+				
+				while(slowPtr != nodeAtStartOfCycle) {
+					count++;
+					slowPtr = slowPtr.getNext();
+				}
+				break;*/
+			}
+		}
+		if(loopExists) {
+			System.out.println("Length of looop: " + count);
+		}
+		
+	}
+}
